627ccd20d5b36669cf4a1c77c28a107d578e759a
Cal <31387289+callumadair@users.noreply.github.com>
2020-11-12 20:41:50 +0000

    Created DailyMessage and laid out attributes and methods.

---
+++ src/menu/DailyMessage.java
@@ -0,0 +1,17 @@
+package menu;
+
+public class DailyMessage {
+    private String todaysRandomMessage;
+
+    public String getTodaysRandomMessage() {
+        return todaysRandomMessage;
+    }
+
+    public void setTodaysRandomMessage(String todaysRandomMessage) {
+        this.todaysRandomMessage = todaysRandomMessage;
+    }
+
+    public void decryptString(String encryptedString) {
+
+    }
+}
