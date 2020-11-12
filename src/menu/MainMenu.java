92f7e543129593e98b8226e9d7f12d64668195ae
Cal <31387289+callumadair@users.noreply.github.com>
2020-11-12 20:36:41 +0000

    Laid out basic menu structure based off the UML

--- src/menu/MainMenu.java
+++ src/menu/MainMenu.java
@@ -1,4 +1,20 @@
 package menu;
 
 public class MainMenu {
+    private boolean isQuit = false;
+
+    public void quitGame() {
+        isQuit = true;
+    }
+
+    public int getHighScore() {
+        return -1;
+    }
+
+    private boolean isInstructionsOpen() {
+        return false;
+    }
+
+    private void startGame() {
+    }
 }
