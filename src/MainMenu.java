0fd1957c367dea2c617ebc57679a786ae0445830
Luke-Young20 <lukegyoung1@gmail.com>
2020-11-16 21:42:13 +0000

    Create MainMenu.java

---
+++ src/MainMenu.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
+import sun.applet.AppletClassLoader;
+
+import static javafx.application.Application.launch;
+
+public class MainMenu extends Application {
+public void start(Stage mainMenu)
+{
+    Button quit =  new Button("quit");
+}
+
+
+
+
+    public static void main(String args[])
+    {
+        // launch the application
+        launch(args);
+    }}
+
+    /*
+    public void quitGame(){
+        System.exit(0);
+    }
+
+     */
