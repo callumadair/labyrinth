f0e0f6aa81bd546086065b6ff221053919dd8fae
Cal <31387289+callumadair@users.noreply.github.com>
2020-11-21 16:32:16 +0000

    Init

--- src/menu/Leaderboard.java
+++ src/menu/Leaderboard.java
@@ -4,18 +4,38 @@ import objects.*;
 
 import java.util.*;
 
+/**
+ * The type Leaderboard.
+ */
 public class Leaderboard {
     private ArrayList<PlayerProfile> playerProfiles;
 
+    /**
+     * Instantiates a new Leaderboard.
+     *
+     * @param database the database
+     */
     public Leaderboard(PlayerDatabase database) {
         setPlayerProfiles(database.getAllData());
     }
 
+    /**
+     * Gets player profiles.
+     *
+     * @return the player profiles
+     */
     public ArrayList<PlayerProfile> getPlayerProfiles() {
         return playerProfiles;
     }
 
+    /**
+     * Sets player profiles.
+     *
+     * @param playerProfiles the player profiles
+     */
     public void setPlayerProfiles(ArrayList<PlayerProfile> playerProfiles) {
         this.playerProfiles = playerProfiles;
+        this.playerProfiles.sort((PlayerProfile p1, PlayerProfile p2) ->
+                p2.getPlayerID().compareTo(p1.getVictories()));
     }
 }
