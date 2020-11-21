049634a16db5a4ff868fd8cd74c4d563b41be630
Cal <31387289+callumadair@users.noreply.github.com>
2020-11-21 17:18:30 +0000

    produced arraylists for printing results

--- src/menu/Leaderboard.java
+++ src/menu/Leaderboard.java
@@ -9,6 +9,9 @@ import java.util.*;
  */
 public class Leaderboard {
     private ArrayList<PlayerProfile> playerProfiles;
+    private ArrayList<String> profileNames;
+    private ArrayList<Integer> profileVictories;
+    private ArrayList<Integer> profileLosses;
 
     /**
      * Instantiates a new Leaderboard.
@@ -37,5 +40,41 @@ public class Leaderboard {
         this.playerProfiles = playerProfiles;
         this.playerProfiles.sort((PlayerProfile p1, PlayerProfile p2) ->
                 p2.getPlayerID().compareTo(p1.getVictories()));
+    }
+
+    public ArrayList<String> getProfileNames() {
+        return profileNames;
+    }
+
+    public void setProfileNames() {
+        ArrayList<String> names = new ArrayList<>();
+        for (PlayerProfile playerProfile : playerProfiles) {
+            names.add(playerProfile.getPlayerName());
+        }
+        this.profileNames = names;
+    }
+
+    public ArrayList<Integer> getProfileVictories() {
+        return profileVictories;
+    }
+
+    public void setProfileVictories() {
+        ArrayList<Integer> victories = new ArrayList<>();
+        for (PlayerProfile playerProfile : playerProfiles) {
+            victories.add(playerProfile.getVictories());
+        }
+        this.profileVictories = victories;
+    }
+
+    public ArrayList<Integer> getProfileLosses() {
+        return profileLosses;
+    }
+
+    public void setProfileLosses(ArrayList<Integer> profileLosses) {
+        ArrayList<Integer> losses = new ArrayList<>();
+        for (PlayerProfile playerProfile : playerProfiles) {
+            losses.add(playerProfile.getVictories());
+        }
+        this.profileLosses  = losses;
     }
 }
