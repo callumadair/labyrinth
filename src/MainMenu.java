e062eb2c5a6066a17b963299332e29dd566262b5
Luke-Young20 <lukegyoung1@gmail.com>
2020-11-21 12:56:59 +0000

    Update MainMenu.java

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -5,13 +5,14 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 import sun.applet.AppletClassLoader;
-
 import static javafx.application.Application.launch;
 
 public class MainMenu extends Application {
 public void start(Stage mainMenu)
 {
+    //    Stage s = new Stage(300, 200, 200);
     Button quit =  new Button("quit");
+    StackPane s1 = new StackPane();
 }
 
 
