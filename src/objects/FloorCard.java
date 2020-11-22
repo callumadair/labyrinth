1db9377f1158e1dde6934f0fb3853dee298243f8
maha-malik <74249791+maha-malik@users.noreply.github.com>
2020-11-22 03:05:01 +0000

    init

--- src/objects/FloorCard.java
+++ src/objects/FloorCard.java
@@ -1,13 +1,21 @@
 package objects;
+/**
+ * This class represents the different floor tiles of the game.
+ * @author Maha Malik
+ * @verson 1.8
+ */
 
 import javafx.scene.image.Image;
 
 public class FloorCard extends Card{
 
     private FloorType type;
-    private boolean isMoveable;
+    private boolean isFixed;
     private FloorTileState state = FloorTileState.NORMAL;
     private int[] openings = new int[4];
+    private int rotation;
+    private int x;
+    private int y;
 
     public enum FloorType {
         STRAIGHT, CORNER, T_SHAPED, GOAL;
@@ -16,6 +24,34 @@ public class FloorCard extends Card{
     public enum FloorTileState{
         FIRE, FROZEN, NORMAL;
     }
+
+    public FloorCard (String type, int rotation) {
+        switch (type) {
+            case "STRAIGHT" :
+                this.type = FloorCard.STRAIGHT;
+                image straight = new image(StraightPath);
+                break;
+            case "CORNER" :
+                this.type = FloorCard.CORNER;
+                image corner = new image(CornerPath);
+                break;
+            case "T_SHAPED" :
+                this.type = FloorCard.T_SHAPED;
+                image tshaped = new image(TShapedPath);
+                break;
+            case "GOAL" :
+                this.type = FloorCard.GOAL;
+                image goal = new image(GoalPATH);
+                break;
+        }
+
+        this.rotation = rotation;
+    }
+
+    public FloorCard () {
+        isFixed = true;
+    }
+
     public FloorTileState state() {
         return state;
     }
@@ -39,16 +75,50 @@ public class FloorCard extends Card{
         return type == FloorType.GOAL;
     }
 
-    public boolean isMoveable() {
-        return isMoveable;
+    public boolean isFixed() {
+        return isFixed;
     }
 
-    public void setMoveable(boolean moveable) {
-        isMoveable = moveable;
+    public void setFixed(boolean fixedTiles) {
+        isFixed = fixedTiles;
     }
 
-    public FloorCard rotateShape(FloorCard floorTile) {
-        return floorTile;
+    public int getRotation (int rotation) {
+        return rotation;
+    }
+
+    public void setRotation () {
+        switch (rotation) {
+            case STRAIGHT:
+                if (rotation = 0 || 180) {
+                    setOpenings(1,0,1,0);
+                } else if (rotation = 90 || 270) {
+                    setOpenings(0,1,0,1);
+                }
+                break;
+            case CORNER:
+                if (rotation = 0) {
+                    setOpenings(0,1,1,0);
+                } else if (rotation = 90) {
+                    setOpenings(0,0,1,1);
+                } else if (rotation = 180) {
+                    setOpenings(1,0,0,1);
+                } else (rotation = 270) {
+                    setOpenings(1, 1, 0, 0);
+                }
+                break;
+            case T_SHAPED:
+                if (rotation = 0) {
+                    setOpenings(1,0,1,1);
+                } else if (rotation = 90) {
+                    setOpenings(1,1,0,1);
+                } else if (rotation = 180) {
+                    setOpenings(1,1,1,0);
+                } else (rotation = 270) {
+                    setOpenings(0, 1, 1, 1);
+                }
+                break;
+        }
     }
 
     public void setOpenings(int left, int top, int right, int bottom) {
