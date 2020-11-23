3ba480035daf12938129df8d0232bfbf8c3581d0
Cal <31387289+callumadair@users.noreply.github.com>
2020-11-23 11:11:20 +0000

    commit

--- src/objects/PlayerController.java
+++ src/objects/PlayerController.java
@@ -2,39 +2,63 @@ package objects;
 
 import java.util.*;
 
-
+/**
+ * The type Player controller.
+ */
 public class PlayerController {
     private int playerX;
     private int playerY;
     private boolean isGoalReached;
     private ArrayList<Card> cardsHeld;
 
-
+    /**
+     * Instantiates a new Player controller.
+     */
     public PlayerController() {
 
     }
 
-
+    /**
+     * Gets player x.
+     *
+     * @return the player x
+     */
     public int getPlayerX() {
         return playerX;
     }
 
-
+    /**
+     * Sets player x.
+     *
+     * @param playerX the player x
+     */
     public void setPlayerX(int playerX) {
         this.playerX = playerX;
     }
 
-
+    /**
+     * Gets player y.
+     *
+     * @return the player y
+     */
     public int getPlayerY() {
         return playerY;
     }
 
-
+    /**
+     * Sets player y.
+     *
+     * @param playerY the player y
+     */
     public void setPlayerY(int playerY) {
         this.playerY = playerY;
     }
-}
-/*
+
+    /**
+     * Is goal reached boolean.
+     *
+     * @return the boolean
+     */
     public boolean isGoalReached() {
         if((Board.getTile((playerX), playerY)).equals(FloorCard.FloorType.GOAL)){
             return isGoalReached;
@@ -43,12 +67,18 @@ public class PlayerController {
         }
     }
 
-
+    /**
+     * Sets goal reached.
+     *
+     * @param goalReached the goal reached
+     */
     public void setGoalReached(boolean goalReached) {
         isGoalReached = goalReached;
     }
 
-
+    /**
+     * Move player.
+     */
     public void movePlayer() {
         if ((Board.getTile((playerX + 1), playerY)).equals(FloorCard.FloorTileState.NORMAL)) { //determine legal moves
             this.playerX += 1;
@@ -64,12 +94,20 @@ public class PlayerController {
         }
     }
 
-
+    /**
+     * Determine legal moves floor card [ ].
+     *
+     * @return the floor card [ ]
+     */
     public FloorCard[] determineLegalMoves() {
         return null;
     }
 
-
+    /**
+     * Draw card card.
+     *
+     * @return the card
+     */
     public Card drawCard() {
         Card card = SilkBag.drawACard();
 
@@ -89,7 +127,9 @@ public class PlayerController {
         }
     }
 
-
+    /**
+     * Use card.
+     */
     public void useCard(Card card){
             if (card.equals(ActionCard.ActionType.IceCard)) {
                 //chooses a tile on board
@@ -107,4 +147,4 @@ public class PlayerController {
 
             }
         }
-}*/
+}
