9b60e48a92928c16383c3d18a673018541e1ce84
Cal <31387289+callumadair@users.noreply.github.com>
2020-11-24 21:35:57 +0000

    Update MenuController.java

--- src/menu/MenuController.java
+++ src/menu/MenuController.java
@@ -1,15 +1,9 @@
 package menu;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
 
 import java.io.IOException;
 
