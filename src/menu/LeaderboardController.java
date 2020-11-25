33137454b93f4843ecb513b0c1ecdebc7b212ff1
Cal <31387289+callumadair@users.noreply.github.com>
2020-11-25 19:38:18 +0000

    Converted leaderboard into an fxml format

--- src/menu/LeaderboardController.java
+++ src/menu/LeaderboardController.java
@@ -42,29 +42,26 @@ public class LeaderboardController extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Scene scene = new Scene(new Group());
         stage = primaryStage;
-        stage.setTitle("Leaderboard");
-        stage.setWidth(350);
-        stage.setHeight(500);
-
-        PlayerDatabase playerDatabase = new PlayerDatabase();
-        playerDatabase.start(getDatabaseName());
-        data = FXCollections.observableArrayList(playerDatabase.getAllActiveProfiles());
-
-        final Label label = new Label("Leaderboard");
-        label.setFont(new Font("Quicksand", 20));
-        addColumns();
-
-        ((Group) scene.getRoot()).getChildren().addAll(createVBox(label));
-
-        stage.setScene(scene);
-        stage.show();
+        Pane root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Leaderboard.fxml"));
+            Scene scene = new Scene(root, 350, 500);
+            PlayerDatabase playerDatabase = new PlayerDatabase();
+            playerDatabase.start(getDatabaseName());
+            data = FXCollections.observableArrayList(playerDatabase.getAllActiveProfiles());
+            stage.setScene(scene);
+            addColumns();
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * Add columns.
      */
+    @FXML
     private void addColumns() {
         TableColumn<PlayerProfile, String> nameCol = new TableColumn<>("Name");
         nameCol.setCellValueFactory(new PropertyValueFactory<>("playerName"));
@@ -76,27 +73,6 @@ public class LeaderboardController extends Application {
         lossColumn.setCellValueFactory(new PropertyValueFactory<>("losses"));
 
         tableView.setItems(data);
-        tableView.getColumns().addAll(nameCol, vicCol, lossColumn);
-        for (TableColumn<PlayerProfile, ?> column : tableView.getColumns()) {
-            column.setMinWidth(100);
-            column.setStyle("-fx-alignment: CENTER;");
-        }
-    }
-
-    /**
-     * Create v box v box.
-     *
-     * @param label the label
-     * @return the v box
-     */
-    private VBox createVBox(Label label) {
-        final VBox vBox = new VBox();
-        vBox.setSpacing(5);
-        vBox.setPadding(new Insets(10, 0, 0, 10));
-        vBox.getChildren().addAll(label, tableView);
-        vBox.setAlignment(Pos.CENTER);
-
-        return vBox;
     }
 
     /**
