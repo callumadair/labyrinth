18d46bc2adaa434f04472cf2b524aa36a762d381
Cal <31387289+callumadair@users.noreply.github.com>
2020-11-26 12:47:12 +0000

    Revert "Update MenuController.java"
    This reverts commit db66848244378ab9304e20ced7d0b61cbc52e773.

--- src/menu/MenuController.java
+++ src/menu/MenuController.java
@@ -66,6 +66,8 @@ public class MenuController extends Application {
     }
 
     /**
+<<<<<<< HEAD
+<<<<<<< HEAD
      * This will take a window that you will be taken to when you click the instructions button
      *
      * @param actionEvent the action event
@@ -87,6 +89,10 @@ public class MenuController extends Application {
 
     /**
      * Handle play button action.
+=======
+>>>>>>> parent of 87c9e37... Update MenuController.java
+=======
+>>>>>>> parent of 87c9e37... Update MenuController.java
      * This will create a second window that you will be taken to when you click the play button
      *
      * @param actionEvent the action event
