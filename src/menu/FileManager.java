8b8b34fb7e2d8618525a954b1ef18be836265d43
Cal <31387289+callumadair@users.noreply.github.com>
2020-11-28 13:50:54 +0000

    created a read format

--- src/menu/FileManager.java
+++ src/menu/FileManager.java
@@ -3,12 +3,13 @@ package menu;
 import objects.*;
 
 import java.io.*;
+import java.util.*;
 
 public class FileManager {
     private String saveFileDirectory;
 
     public FileManager(String saveFileDirectory) {
-        this.saveFileDirectory = saveFileDirectory;
+        setSaveFileDirectory(saveFileDirectory);
     }
 
     public String getSaveFileDirectory() {
@@ -33,9 +34,8 @@ public class FileManager {
             for (int j = 0; j < board.getSpawnPoints()[i].length; j++) {
                 fileWriter.write(board.getSpawnPoints()[i][j] + " ");
             }
-            fileWriter.write("\n");
         }
-
+        fileWriter.write("\n");
 
         fileWriter.write(board.getFixedTilesNum() + "\n");
         for (int k = 0; k < board.getFixedTiles().length; k++) {
@@ -44,6 +44,62 @@ public class FileManager {
             fileWriter.write(board.getTile(x, y).getType().toString() + " " + x + " " + y + " "
                     + board.getTile(x, y).getRotation());
         }
+        fileWriter.write("\n");
         //add non fixed cards
+    }
+
+    public void loadBoard(int boardNum) throws FileNotFoundException {
+        File boardFile = new File(getSaveFileDirectory() + "board" + boardNum + ".txt");
+        Scanner scanner = new Scanner(boardFile);
+
+        int width = scanner.nextInt();
+        int length = scanner.nextInt();
+        scanner.nextLine();
+
+        int[][] spawnPoints = new int[4][2];
+        for (int i = 0; i < spawnPoints.length; i++) {
+            for (int j = 0; j < spawnPoints[i].length; j++) {
+                spawnPoints[i][j] = scanner.nextInt();
+            }
+        }
+        scanner.nextLine();
+        int numFixed = scanner.nextInt();
+        FloorCard[] fixed = new FloorCard[numFixed];
+        for (int k = 0; k < fixed.length; k++) {
+            String type = scanner.next();
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            int rotation = scanner.nextInt();
+            fixed[k] = new FloorCard(type, x, y, rotation);
+        }
+        ArrayList<Card> silkBagCards = new ArrayList<>();
+        createFloorCards(scanner.nextInt(), "STRAIGHT", silkBagCards);
+        createFloorCards(scanner.nextInt(), "CORNER", silkBagCards);
+        createFloorCards(scanner.nextInt(), "T_SHAPED", silkBagCards);
+        createFloorCards(scanner.nextInt(), "GOAL", silkBagCards); //needed?
+        scanner.nextLine();
+        silkBagCards.addAll(createActionCards(scanner.nextInt(), "FIRE"));
+        silkBagCards.addAll(createActionCards(scanner.nextInt(), "ICE"));
+        silkBagCards.addAll(createActionCards(scanner.nextInt(), "BACKTRACK"));
+        silkBagCards.addAll(createActionCards(scanner.nextInt(), "DOUBLE_MOVE"));
+
+        SilkBag silkBag = new SilkBag(silkBagCards.size());
+        silkBag.setListOfCards(silkBagCards);
+
+
+    }
+
+    private void createFloorCards(int num, String type, ArrayList<Card> cards) {
+        for (int i = 0; i < num; i++) {
+            cards.add(new FloorCard(type));
+        }
+    }
+
+    private ArrayList<Card> createActionCards(int num, String type) {
+        ArrayList<Card> cards = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            cards.add(new ActionCard(type));
+        }
+        return cards;
     }
 }
