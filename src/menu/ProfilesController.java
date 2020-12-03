6fe7ea7899968f4a26c8fc51f02e4f28e62ef62b
xLisu <pocztalis@icloud.com>
2020-12-03 18:06:51 +0100

    Fixes

---
+++ src/menu/ProfilesController.java
@@ -0,0 +1,26 @@
+package menu;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import objects.*;
+
+import java.util.*;
+
+public class ProfilesController extends Application {
+    private ArrayList<PlayerDatabase> databases = new ArrayList<>();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+    public ArrayList<PlayerProfile> getProfiles() {
+        ArrayList<PlayerProfile> profiles = new ArrayList<>();
+        for (PlayerDatabase database : databases) {
+            profiles.addAll(database.getAllData());
+        }
+    }
+}
