0973521325d339aef2110a24bbbb23389c04d3b1
Cal <31387289+callumadair@users.noreply.github.com>
2020-12-04 18:44:51 +0000

    Tidying and bug fix

--- src/menu/MenuController.java
+++ src/menu/MenuController.java
@@ -39,6 +39,10 @@ public class MenuController extends Application implements Initializable {
     private ImageView imageView;
     @FXML
     private Button musicOnOffButton;
+    @FXML
+    private Button playButton;
+    @FXML
+    private Button quitButton;
     private static MediaPlayer menuMusic;
     private ArrayList<PlayerDatabase> databases = new ArrayList<>();
 
@@ -50,6 +54,61 @@ public class MenuController extends Application implements Initializable {
     public static void main(String[] args) {
         playMusic("src\\resources\\MenuMusic.wav");
         launch(args);
+    }
+
+    /**
+     * Will setup the background and the dailyMessage on the main menu screen
+     *
+     * @param location
+     * @param resources
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            textLabelID.setText(MessageOfTheDay.finalMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        imageView.fitWidthProperty().bind(stackPane.widthProperty().multiply(1.1));
+        imageView.fitHeightProperty().bind(stackPane.heightProperty().multiply(1.1));
+        imageView.setPreserveRatio(false);
+
+        TranslateTransition backgroundMove = new TranslateTransition();
+
+        backgroundMove.setDuration(Duration.millis(5000));
+        backgroundMove.setNode(imageView);
+        backgroundMove.setFromX(0);
+        backgroundMove.setToX(30);
+        backgroundMove.setAutoReverse(true);
+        backgroundMove.setCycleCount(Animation.INDEFINITE);
+        backgroundMove.play();
+    }
+
+    /**
+     * Make fade out.
+     *
+     * @param fadeOut the fade out
+     */
+    private void fadeOut(Pane fadeOut) {
+        TranslateTransition windowTransition = new TranslateTransition();
+        windowTransition.setDuration(Duration.millis(500));
+        windowTransition.setNode(fadeOut);
+        windowTransition.setFromX(700);
+        windowTransition.setToX(0);
+        windowTransition.play();
+    }
+
+    /**
+     * Will play the music on the main menu screen
+     *
+     * @param filepath
+     */
+    public static void playMusic(String filepath) {
+        Media music = new Media(new File(filepath).toURI().toString());
+        menuMusic = new MediaPlayer(music);
+        menuMusic.setCycleCount(MediaPlayer.INDEFINITE);
+        menuMusic.play();
     }
 
     /**
@@ -94,8 +153,9 @@ public class MenuController extends Application implements Initializable {
     private void handlePlayButtonAction(ActionEvent actionEvent) {
         borderPane.getChildren().remove(mainView);
         try {
-            Pane root = FXMLLoader.load(MenuController.class.getResource("AnotherPlay.fxml"));
-            borderPane.setCenter(root);
+            mainView = FXMLLoader.load(MenuController.class.getResource("/menu/AnotherPlay.fxml"));
+            fadeOut(mainView);
+            borderPane.setCenter(mainView);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -103,23 +163,6 @@ public class MenuController extends Application implements Initializable {
 
     }
 
-    /**
-     * This will take a window that you will be taken to when you click the instructions button
-     *
-     * @param actionEvent the action event
-     */
-    @FXML
-    private void handleInstructionsButtonAction(ActionEvent actionEvent) {
-        try {
-            mainView = FXMLLoader.load(getClass().getResource("Instructions.fxml"));
-            Scene instructionsScene = new Scene(mainView);
-            stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            stage.setScene(instructionsScene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
      * Handle quit button action.
@@ -146,9 +189,21 @@ public class MenuController extends Application implements Initializable {
             mainView = (Pane) ((BorderPane) menu.getChildren().get(1)).getChildren().get(0);
             fadeOut(mainView);
             borderPane.setCenter(mainView);
+
+            playButton = (Button) ((VBox) mainView.getChildren().get(0)).getChildren().get(0);
+            playButton.setOnAction(this::handlePlayButtonAction);
+
+            quitButton = (Button) ((VBox) mainView.getChildren().get(0)).getChildren().get(1);
+            quitButton.setOnAction(this::handleQuitButtonAction);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    private void handleNewGame(ActionEvent actionEvent) {
+        //String boardName = ((TextField) ((HBox) actionEvent.getSource()).getChildren().get(1)).getText();
+        //System.out.println(boardName);
     }
 
     /**
@@ -174,58 +229,5 @@ public class MenuController extends Application implements Initializable {
 
     }
 
-    /**
-     * Will setup the background and the dailyMessage on the main menu screen
-     *
-     * @param location
-     * @param resources
-     */
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        try {
-            textLabelID.setText(MessageOfTheDay.finalMessage());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        imageView.fitWidthProperty().bind(stackPane.widthProperty().multiply(1.1));
-        imageView.fitHeightProperty().bind(stackPane.heightProperty().multiply(1.1));
-        imageView.setPreserveRatio(false);
-        
-        TranslateTransition backgroundMove = new TranslateTransition();
-
-        backgroundMove.setDuration(Duration.millis(5000));
-        backgroundMove.setNode(imageView);
-        backgroundMove.setFromX(0);
-        backgroundMove.setToX(30);
-        backgroundMove.setAutoReverse(true);
-        backgroundMove.setCycleCount(Animation.INDEFINITE);
-        backgroundMove.play();
-    }
-
-    /**
-     * Make fade out.
-     *
-     * @param fadeOut the fade out
-     */
-    private void fadeOut(Pane fadeOut) {
-        TranslateTransition windowTransition = new TranslateTransition();
-        windowTransition.setDuration(Duration.millis(500));
-        windowTransition.setNode(fadeOut);
-        windowTransition.setFromX(700);
-        windowTransition.setToX(0);
-        windowTransition.play();
-    }
-
-    /**
-     * Will play the music on the main menu screen
-     *
-     * @param filepath
-     */
-    public static void playMusic(String filepath) {
-        Media music = new Media(new File(filepath).toURI().toString());
-        menuMusic = new MediaPlayer(music);
-        menuMusic.setCycleCount(MediaPlayer.INDEFINITE);
-        menuMusic.play();
-    }
 }
