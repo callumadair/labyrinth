a436d9a5a878b09ccc630ddd935d7f6d1048b6bd
Cal <callum.adair@outlook.com>
2020-12-04 11:32:46 +0000

    Update Profiles.java

--- src/menu/Profiles.java
+++ src/menu/Profiles.java
@@ -6,14 +6,25 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import objects.*;
 
+import java.io.*;
 import java.util.*;
 
 public class Profiles {
 
     @FXML
-    private final Button deleteButton = new Button();
+    private final Button deleteButton = new Button("Delete");
+    @FXML
+    private static TableView<PlayerProfile> tableView = new TableView<>();
 
     public static BorderPane getAllProfiles(ArrayList<PlayerDatabase> databases) {
+        BorderPane root = null;
+        try {
+            root = FXMLLoader.load(Profiles.class.getResource("Profiles.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tableView = (TableView<PlayerProfile>) root.getCenter();
+
         HashMap<Integer, PlayerProfile> allProfiles = new HashMap<>();
         for (PlayerDatabase database : databases) {
             database.start();
@@ -27,14 +38,31 @@ public class Profiles {
                 }
             }
         }
-        ObservableMap<Integer, PlayerProfile> profiles = FXCollections.observableHashMap();
-        profiles.putAll(allProfiles);
+        ObservableList<PlayerProfile> profiles = FXCollections.observableArrayList(
+                new ArrayList<>(allProfiles.values()));
+        //addColumns(profiles);
 
-        addColumns();
-        return null;
+        return root;
     }
 
-    private static void addColumns() {
+    /*
+    private static void addColumns(ObservableList<PlayerProfile> profiles) {
+        tableView.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("playerName"));
+        tableView.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("playerID"));
+        tableView.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("gamesPlayed"));
+        tableView.getColumns().get(3).setCellValueFactory(param -> new TableCell<PlayerProfile, PlayerProfile>() {
+            private final Button deleteButton = new Button("Delete");
+            @Override
+            protected void updateItem(PlayerProfile profile, boolean empty) {
+                super.updateItem(profile, empty);
 
-    }
+                if (profile == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(deleteButton);
+                deleteButton.setOnAction(event -> profiles.remove(profile));
+            }
+        });
+    }*/
 }
