82d713bfba5c5316f68b227b95555f435f46abd8
Cal <31387289+callumadair@users.noreply.github.com>
2020-12-06 12:02:27 +0000

    update

--- src/objects/Controller.java
+++ src/objects/Controller.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 
 /**
  * This class represents the Controller.
+ *
  * @author
  */
 public class Controller {
@@ -71,8 +72,8 @@ public class Controller {
      */
     public void startGame() {
         numOfPlayers = players.size() - 1;
-        for(PlayerController player : players){
-            if(player.isCurrentPlayer()){
+        for (PlayerController player : players) {
+            if (player.isCurrentPlayer()) {
                 this.playerIndex = player.getPlayerIndex();
             }
         }
@@ -147,22 +148,22 @@ public class Controller {
         currentPlayerIndex.set(currentPlayer.getPlayerIndex());
         currentPlayer.setCurrentPlayer(!currentPlayer.isCurrentPlayer());
 
-        if(!board.getFrozenTiles().isEmpty()){
+        if (!board.getFrozenTiles().isEmpty()) {
             ArrayList<FloorCard> frozenTilesToRemove = new ArrayList<>();
-            for(FloorCard card : board.getFrozenTiles()){
+            for (FloorCard card : board.getFrozenTiles()) {
                 card.decrementEffectTimer();
-                if(!card.isEffectActive()){
+                if (!card.isEffectActive()) {
                     frozenTilesToRemove.add(card);
                 }
             }
             board.getFrozenTiles().removeAll(frozenTilesToRemove);
         }
 
-        if(!board.getTilesOnFire().isEmpty()){
+        if (!board.getTilesOnFire().isEmpty()) {
             ArrayList<FloorCard> tilesOnFireToRemove = new ArrayList<>();
-            for(FloorCard card : board.getTilesOnFire()){
+            for (FloorCard card : board.getTilesOnFire()) {
                 card.decrementEffectTimer();
-                if(!card.isEffectActive()){
+                if (!card.isEffectActive()) {
                     tilesOnFireToRemove.add(card);
                 }
             }
@@ -184,7 +185,7 @@ public class Controller {
     private void getInsertionList() {
         tilesToCompare = board.getInsertionPoints();
 
-        if(tilesToCompare.isEmpty()){
+        if (tilesToCompare.isEmpty()) {
             playingCard = null;
             changeState(GameState.ACTION_CARD);
         } else {
@@ -254,6 +255,7 @@ public class Controller {
             highlightTiles();
         }
     }
+
     /**
      * Moves the player.
      */
@@ -318,6 +320,7 @@ public class Controller {
                 player.getProfile().incrementLoses();
             }
         }
+        players.add(new PlayerController(null, 0));
     }
 
     /**
