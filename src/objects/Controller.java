42e1afa538168c83351a228ae94b09ee168f5643
xLisu <pocztalis@icloud.com>
2020-12-06 12:38:07 +0100

    Fixed turn

--- src/objects/Controller.java
+++ src/objects/Controller.java
@@ -71,12 +71,17 @@ public class Controller {
      */
     public void startGame() {
         numOfPlayers = players.size() - 1;
+        boolean currentPlayerSet = false;
         for(PlayerController player : players){
             if(player.isCurrentPlayer()){
                 this.playerIndex = player.getPlayerIndex();
+                currentPlayerSet = true;
             }
         }
         currentPlayer = players.get(playerIndex);
+        if(!currentPlayerSet){
+            currentPlayer.setCurrentPlayer(!currentPlayer.isCurrentPlayer());
+        }
         currentPlayerIndex = new SimpleIntegerProperty(playerIndex);
         cardSelectionFlag = new SimpleBooleanProperty(false);
         stateChangeFlag = new SimpleBooleanProperty(false);
@@ -145,7 +150,6 @@ public class Controller {
     private void drawCard() {
         setPlayingCard(board.getSilkBag().drawACard());
         currentPlayerIndex.set(currentPlayer.getPlayerIndex());
-        currentPlayer.setCurrentPlayer(!currentPlayer.isCurrentPlayer());
 
         if(!board.getFrozenTiles().isEmpty()){
             ArrayList<FloorCard> frozenTilesToRemove = new ArrayList<>();
@@ -304,6 +308,7 @@ public class Controller {
         currentPlayer.setCurrentPlayer(!currentPlayer.isCurrentPlayer());
 
         currentPlayer = players.get(playerIndex);
+        currentPlayer.setCurrentPlayer(!currentPlayer.isCurrentPlayer());
         changeState(GameState.DRAWING);
     }
 
@@ -332,11 +337,15 @@ public class Controller {
                 selectedTile = board.getTileFromCanvas(x, y);
                 System.out.println("x: " + selectedTile.getX() + " y: " + selectedTile.getY() +
                         " | " + selectedTile.getType() + " | " + currentState);
-                System.out.println(currentPlayer.getPlayerIndex());
                 System.out.println("Left: " + selectedTile.getOpeningAt(FloorCard.Direction.LEFT) +
                         " Up: " + selectedTile.getOpeningAt(FloorCard.Direction.UP) +
                         " Right: " + selectedTile.getOpeningAt(FloorCard.Direction.RIGHT) +
                         " Down: " + selectedTile.getOpeningAt(FloorCard.Direction.DOWN));
+                for(PlayerController playerController : players){
+                    if(playerController.isCurrentPlayer()){
+                        System.out.println(playerController.getPlayerIndex());
+                    }
+                }
                 playState();
             }
         });
